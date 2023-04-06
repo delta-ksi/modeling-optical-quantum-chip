acb4c9931ad0ed21aa14add2fd25e91226764950
delta_ksi <delta.ksi42@gmail.com>
2023-04-06 19:35:29 +0300

    Launch script

--- launch.sh
+++ launch.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Флаги запуска отдельных
-# модулей скрипта
+#@ Сделать всё через пул действий
+
+# Флаги запуска отдельных модулей скрипта
 tomf=false
 tesf=false
+
+# Создание директории с информацией о томографии
+if [ "$(find -name 'data')" = '' ]; then
+	mkdir data
+fi
 
 # Обработка опций скрипта
 if [ $# -ne 0 ]; then
@@ -11,28 +17,79 @@ if [ $# -ne 0 ]; then
 	do
 		case $arg in
 			# Тестирование
-			-tes)
+			-ts|--test)
 				tesf=true 
 			;;
-			--testing)
-				tesf=true 
-			;;
+
 			# Томография
-			-tom)
+			-tm|--tomography)
 				tomf=true 
 			;;
-			--tomography)
-				tomf=true 
+
+			# Удаление последнего файла
+			-d|--delete)
+				# Проверка, что пользователь действительно
+				# хочет удалить файл
+				flag=false
+				read -p "WARNING::LAUNCH: You want to delete the last file in ./data? (yes/no): " ans
+
+				case $ans in
+				y|yes)
+					flag=true
+				;;
+				*)
+					echo "Cansel deleting. Exit script."
+					exit 0
+				;;
+				esac
+
+				if [ "$flag" = true ]; then
+					# Нахождение последнего по времени созданного файла
+					data_pass=$(ls ./data --sort time | head -n 1)
+
+					# Обработка условия отсутствия хоть какого-нибудть файла данных
+					if [ $data_pass != '' ]; then
+						rm -f ./data/$data_pass
+					else
+						echo "ERROR::LAUNCH: Don't find any data file!"
+						exit -1
+					fi
+				fi
 			;;
+
+			-D|--deleteall)
+				# Проверка, что пользователь действительно
+				# хочет удалить файл
+				flag=false
+				read -p "WARNING::LAUNCH: You want to delete ALL files in ./data? (yes/no): " ans
+
+				case $ans in
+				y|yes)
+					flag=true
+				;;
+				*)
+					echo "Cansel deleting. Exit script."
+					exit 0
+				;;
+				esac
+
+				if [ "$flag" = true ]; then
+					rm -f ./data/*
+				fi
+			;;
+
 			# Мануал
-			--help)
+			-h|--help)
 				echo "-tes or --testing: Using for testing tomographed chip (TC). It builds histogram, and adds to file new information about TC."
 				echo "-tom or --tomography: Using for tomography chip. It uses method dif evolution for findng fixed parameters of transformations of chip."
+				echo "-d or --delete: Delete the last file in ./data."
+				echo "-D or --deleteall: Delete all files in ./data."
 				exit 0
 			;;
+
 			# Ошибка
 			*)
-				echo "Error: Unknow option or variable $arg, please use --help for more information about script options!"
+				echo "ERROR::LAUNCH: Unknow option $arg, please use --help for more information about script options!"
 				exit -1
 			;;
 		esac
@@ -40,11 +97,6 @@ if [ $# -ne 0 ]; then
 else
 	tomf=true
 	tesf=true
-fi
-
-# Создание директории с информацией о томографии
-if [ "$(find -name 'data')" = '' ]; then
-	mkdir data
 fi
 
 # Томография
@@ -63,20 +115,20 @@ if [ "$tesf" = true ]; then
 	data_pass=$(ls ./data --sort time | head -n 1)
 	
 	# Обработка условия отсутствия хоть какого-нибудть файла данных
-	if [ $data_pass != '' ]; then
+	if [ "$data_pass" != '' ]; then
 		# Запуск скрипта на тестирование стомографированного чипа.
 		# Строит графики гистрограмм и дополняет .dat файл
 		# новой статистической информацией
 		echo "Data file ./data/$data_pass:"
 		echo "Launch testing script..."
-		python3 -B testing.py "./data/$data_pass" &
+		python3 -B test.py "./data/$data_pass" &
 	else
-		echo "Error: Don't find any data file!"
+		echo "ERROR::LAUNCH: Don't find any data file!"
 		exit -1
 	fi
 fi
 
-# Удаление вспомогательных файлов
+# Удаление вспомогательных файлов python
 if [ "$(find -name '__pycache__')" != '' ]; then
 	rm -r __pycache__
 fi
